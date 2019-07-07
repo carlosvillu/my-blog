8b48725c736a36c2dec8b5a21bdc9d8f9608aaba
Carlos Villuendas <carlosvillu@gmail.com>
2019-07-07 19:12:13 +0200

    fix lint

--- src/app.js
+++ src/app.js
@@ -6,13 +6,15 @@ import App from './components/App'
 import './index.scss'
 
 const render = () => ReactDOM.render(<App />, document.getElementById('app'))
-
+render()
+;(async () => {
   if ('serviceWorker' in navigator) {
     try {
       const registration = await navigator.serviceWorker.register(
         '/service-worker.js'
       )
       registration.addEventListener('updatefound', () => {
+        // eslint-disable-next-line no-unused-vars
         const installingWorker = registration.installing
         // console.log('Encontrada nueva versión del worker', installingWorker)
         // installingWorker.addEventListener('statechange', evt =>
@@ -20,6 +22,7 @@ const render = () => ReactDOM.render(<App />, document.getElementById('app'))
         // )
       })
     } catch (e) {
+      // eslint-disable-next-line no-console
       console.log('Error al registrar el services worker')
     }
   }
